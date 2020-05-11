7ae8072454ee2e11bea21cebf697b79cad663d45
Cédric Belin <cedric@belin.io>
2020-05-11 11:35:35 +0200

    Added the license

--- tool/version.ps1
+++ tool/version.ps1
@@ -13,6 +13,7 @@ function Update-File {
 }
 
 $version = (Get-Content haxelib.json | ConvertFrom-Json).version
+Update-File composer.json '"version": "\d+(\.\d+){2}"' """version"": ""$version"""
 Update-File package.json '"version": "\d+(\.\d+){2}"' """version"": ""$version"""
 Update-File README.md 'release-v\d+(\.\d+){2}' "release-v$version"
 Update-File doc/index.md 'release-v\d+(\.\d+){2}' "release-v$version"
