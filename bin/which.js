ade4b0fd6683b9abeaf606515120d537e54a81d1
Cédric Belin <cedric@belin.io>
2020-05-11 11:51:12 +0200

    Added the CLI scripts

---
+++ bin/which.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+import {FinderException, Program} from '../lib/index.mjs';
+
+// Start the application.
+Program.main().catch(err => {
+  if (err instanceof FinderException) process.exitCode = 1;
+  else {
+    console.error(err.message);
+    process.exitCode = 2;
+  }
+});
