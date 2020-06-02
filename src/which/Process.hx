a74ecd316a2b37a7e90dc042b6900e66176ba593
Cédric Belin <cedric@belin.io>
2020-06-02 17:19:01 +0200

    Fixed a bug

--- src/which/Process.hx
+++ src/which/Process.hx
@@ -1,7 +1,6 @@
 package which;
 
 import thenshim.Promise;
-using StringTools;
 
 #if nodejs
 import js.Lib.typeof;
@@ -10,7 +9,6 @@ import js.node.ChildProcess;
 import js.node.Util;
 #elseif php
 import php.Global.function_exists;
-import php.Syntax;
 #end
 
 /** Provides information about the current process. **/
@@ -27,7 +25,7 @@ class Process {
 		#if nodejs
 		if (typeof(Node.process.getgid) == "function") return Promise.resolve(Node.process.getgid());
 		#elseif php
-		if (function_exists("posix_getgid")) return Promise.resolve(Syntax.code("posix_getgid()"));
+		if (function_exists("posix_getgid")) return Promise.resolve(php.Syntax.code("posix_getgid()"));
 		#end
 		return getProcessId(ProcessIdentity.group);
 	}
@@ -37,7 +35,7 @@ class Process {
 		#if nodejs
 		if (typeof(Node.process.getuid) == "function") return Promise.resolve(Node.process.getuid());
 		#elseif php
-		if (function_exists("posix_getuid")) return Promise.resolve(Syntax.code("posix_getuid()"));
+		if (function_exists("posix_getuid")) return Promise.resolve(php.Syntax.code("posix_getuid()"));
 		#end
 		return getProcessId(ProcessIdentity.user);
 	}
@@ -48,12 +46,15 @@ class Process {
 
 		#if nodejs
 		final exec = Util.promisify(ChildProcess.exec);
-		return exec('id -$identity').then(process -> Std.parseInt(process.stdout.rtrim()));
+		return exec('id -$identity').then(process -> {
+			final id = Std.parseInt(process.stdout);
+			id != null ? id : -1;
+		});
 		#else
 		final process = new sys.io.Process("id", ['-$identity']);
-		final id = process.exitCode() != 0 ? -1 : Std.parseInt(process.stdout.readLine());
+		final id = process.exitCode() != 0 ? null : Std.parseInt(process.stdout.readLine());
 		process.close();
-		return Promise.resolve(id);
+		return Promise.resolve(id != null ? id : -1);
 		#end
 	}
 }
