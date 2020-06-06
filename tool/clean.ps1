1244b284caac2944a8f5b290ff12672468e39089
Cédric Belin <cedric@belin.io>
2020-06-06 23:15:20 +0200

    Updated the "clean" task

--- tool/clean.ps1
+++ tool/clean.ps1
@@ -2,8 +2,9 @@
 Set-StrictMode -Version Latest
 Set-Location (Split-Path $PSScriptRoot)
 
-foreach ($item in ".temp", "doc/api", "lib/index.php", "lib/js/which.*", "lib/php", "lib/res", "run.n", "www") {
+foreach ($item in ".temp", "doc/api", "lib/index.php", "lib/js/which.*", "lib/res", "run.n", "www") {
 	if (Test-Path $item) { Remove-Item $item -Force -Recurse }
 }
 
+Remove-Item lib/php/* -Exclude which.php -Recurse
 Get-ChildItem var -Exclude .gitkeep | Remove-Item -Recurse
