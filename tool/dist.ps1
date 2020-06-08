305281ddb4c99d40682e5747cc29d7da5df607b5
Cédric Belin <cedric@belin.io>
2020-06-08 23:55:30 +0200

    Updated the build system

--- tool/dist.ps1
+++ tool/dist.ps1
@@ -6,7 +6,7 @@ tool/clean.ps1
 tool/version.ps1
 tool/build.ps1
 
-node_modules/.bin/ncc build bin/which.js --minify --out=var
+npm run dist
 @("#!/usr/bin/env node") + (Get-Content var/index.js) | Out-File bin/which.js
 if (-not $IsWindows) { chmod +x bin/which.js }
 
--- tool/upgrade.ps1
+++ tool/upgrade.ps1
@@ -2,10 +2,6 @@
 Set-StrictMode -Version Latest
 Set-Location (Split-Path $PSScriptRoot)
 
-git reset --hard
-git fetch --all --prune
-git pull --rebase
-
 composer update --no-interaction
 haxelib install all --always
 haxelib update --always
