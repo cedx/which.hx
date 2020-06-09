e7c551c1371f06d9491c5f9787d7f685803ab13f
Cédric Belin <cedric@belin.io>
2020-06-09 17:19:19 +0200

    Updated the documentation

--- src/Version.hx
+++ src/Version.hx
@@ -2,14 +2,14 @@ import haxe.Json;
 import haxe.macro.Expr.ExprOf;
 import sys.io.File;
 
-/** Provides the version number of this package. **/
+/** Provides information about the program version. **/
 class Version {
 
-	/** Reads the version number of this package from the `haxelib.json` file. **/
+	/** Reads the package version from the `haxelib.json` file. **/
 	public static function getHaxelibVersion(): String
 		return try Json.parse(File.getContent("haxelib.json")).version catch (e) "0.0.0";
 
-	/** Gets the version number of this package. **/
-	public static macro function getPackageVersion(): ExprOf<String>
+	/** Gets the package version of this program. **/
+	macro public static function getPackageVersion(): ExprOf<String>
 		return macro $v{#if display "0.0.0" #else getHaxelibVersion() #end};
 }
