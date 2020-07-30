aaf5b189d117db96ae605cfc4ba8a08c6adb11fb
Cédric Belin <cedric@belin.io>
2020-07-30 17:30:05 +0200

    Added the `Version.getHaxeVersion()` method

--- src/Version.hx
+++ src/Version.hx
@@ -1,14 +1,19 @@
 import haxe.Json;
+import haxe.macro.Context;
 import sys.io.File;
 
 /** Provides information about the program version. **/
 class Version {
 
-	/** Reads the package version from the `haxelib.json` file. **/
-	public static function getHaxelibVersion(): String
-		return try Json.parse(File.getContent("haxelib.json")).version catch (e) "0.0.0";
+	/** Gets the version of the Haxe compiler. **/
+	macro public static function getHaxeVersion(): ExprOf<String>
+		return macro $v{Context.definedValue("haxe")};
 
 	/** Gets the package version of this program. **/
 	macro public static function getPackageVersion(): ExprOf<String>
-		return macro $v{#if display "0.0.0" #else getHaxelibVersion() #end};
+		return macro $v{#if display "0.0.0" #else getHaxelibFileVersion() #end};
+
+	/** Reads the package version from the `haxelib.json` file. **/
+	static function getHaxelibFileVersion(): String
+		return try Json.parse(File.getContent("haxelib.json")).version catch (e) "0.0.0";
 }
