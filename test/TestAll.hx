8f236c386ce287075fd59aa194a5b7834b243a4e
Cédric Belin <cedric@belin.io>
2020-07-30 12:27:19 +0200

    Replaced "mcover" by "instrument" for code coverage

--- test/TestAll.hx
+++ test/TestAll.hx
@@ -1,5 +1,4 @@
-import mcover.coverage.MCoverage;
-import mcover.coverage.client.LcovPrintClient;
+import instrument.coverage.Coverage;
 import utest.UTest;
 import which.*;
 
@@ -14,9 +13,5 @@ class TestAll {
 	];
 
 	/** Application entry point. **/
-	static function main() UTest.run(tests, () -> {
-		final logger = MCoverage.getLogger();
-		logger.addClient(new LcovPrintClient("which", "var/lcov.info"));
-		logger.report();
-	});
+	static function main() UTest.run(tests, Coverage.endCoverage);
 }
