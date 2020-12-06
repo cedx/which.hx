4976757e1ae8b7e4ead7d1600c6494dd5273ecc9
Cédric Belin <cedric@belin.io>
2020-12-06 19:30:20 +0100

    Updated the build system

--- tool/clean.ps1
+++ tool/clean.ps1
@@ -2,7 +2,7 @@
 Set-StrictMode -Version Latest
 Set-Location (Split-Path $PSScriptRoot)
 
-foreach ($item in "doc/api", "lib/index.php", "lib/js/which.*", "lib/res", "www") {
+foreach ($item in "docs/api", "lib/index.php", "lib/js/which.*", "lib/res") {
 	if (Test-Path $item) { Remove-Item $item -Force -Recurse }
 }
 
--- tool/doc.ps1
+++ tool/doc.ps1
@@ -6,15 +6,13 @@ $version = (Get-Content haxelib.json | ConvertFrom-Json).version
 haxe build_doc.hxml
 haxelib run dox `
 	--define description "Find the instances of an executable in the system path, in Haxe, JavaScript and PHP. Like the `which` Linux command." `
-	--define logo "https://api.belin.io/which.hx/favicon.ico" `
 	--define source-path "https://git.belin.io/cedx/which.hx/src/branch/main/src" `
 	--define themeColor 0xffc105 `
 	--define version $version `
 	--define website "https://belin.io" `
 	--input-path var `
-	--output-path doc/api `
+	--output-path docs/api `
 	--title "Which.hx" `
 	--toplevel-package which
 
-Copy-Item doc/img/favicon.ico doc/api
-mkdocs build --config-file=etc/mkdocs.yaml
+Copy-Item docs/favicon.ico docs/api
