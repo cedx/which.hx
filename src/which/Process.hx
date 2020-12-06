4f48a2c27d45e26e46b1c4531272013efd8505a5
Cédric Belin <cedric@belin.io>
2020-12-06 19:38:30 +0100

    Ported the `Process` class to Tinkerbell promises

--- src/which/Process.hx
+++ src/which/Process.hx
@@ -1,12 +1,13 @@
 package which;
 
-import thenshim.Promise;
+import asys.io.Process as AsyncProcess;
+
+using StringTools;
+using tink.io.Source;
 
 #if nodejs
 import js.Lib.typeof;
 import js.Node;
-import js.node.ChildProcess;
-import js.node.Util;
 #elseif php
 import php.Global.function_exists;
 import php.Syntax;
@@ -22,40 +23,32 @@ class Process {
 	public static var uid(get, never): Promise<Int>;
 
 	/** Gets the identifier of the current process's group. **/
-	static function get_gid() {
-		#if nodejs
-		if (typeof(Node.process.getgid) == "function") return Promise.resolve(Node.process.getgid());
-		#elseif php
-		if (function_exists("posix_getgid")) return Promise.resolve(Syntax.code("posix_getgid()"));
-		#end
+	static function get_gid(): Promise<Int> {
+		#if nodejs if (typeof(Node.process.getgid) == "function") return Node.process.getgid();
+		#elseif php if (function_exists("posix_getgid")) return Syntax.code("posix_getgid()"); #end
 		return getProcessId(Group);
 	}
 
 	/** Gets the identifier of the current process's user. **/
-	static function get_uid() {
-		#if nodejs
-		if (typeof(Node.process.getuid) == "function") return Promise.resolve(Node.process.getuid());
-		#elseif php
-		if (function_exists("posix_getuid")) return Promise.resolve(Syntax.code("posix_getuid()"));
-		#end
+	static function get_uid(): Promise<Int> {
+		#if nodejs if (typeof(Node.process.getuid) == "function") return Node.process.getuid();
+		#elseif php if (function_exists("posix_getuid")) return Syntax.code("posix_getuid()"); #end
 		return getProcessId(User);
 	}
 
 	/** Gets the numeric `identity` of the current process. **/
 	static function getProcessId(identity: ProcessIdentity): Promise<Int> {
-		if (Finder.isWindows) return Promise.resolve(-1);
+		if (Finder.isWindows) return Failure(new Error("Not supported on Windows platform."));
 
-		#if nodejs
-			final exec = Util.promisify(ChildProcess.exec);
-			return exec('id -$identity').then(process -> {
-				final id = Std.parseInt(process.stdout);
-				id != null ? id : -1;
+		final process = new AsyncProcess("id", ['-$identity']);
+		return process.exitCode()
+			.next(exitCode -> exitCode != 0 ? Failure(new Error("Process exited with a non-zero code.")) : process.stdout.all().next(chunk -> {
+				final id = Std.parseInt(chunk.toString().rtrim());
+				id != null ? Success(id) : Failure(new Error("Unable to parse the process output."));
+			}))
+			.next(processId -> {
+				process.close();
+				processId;
 			});
-		#else
-			final process = new sys.io.Process("id", ['-$identity']);
-			final id = process.exitCode() != 0 ? null : Std.parseInt(process.stdout.readLine());
-			process.close();
-			return Promise.resolve(id != null ? id : -1);
-		#end
 	}
 }
