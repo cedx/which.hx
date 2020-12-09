ca0461367911bd2d57980e9f8a2fdd143459220b
Cédric Belin <cedric@belin.io>
2020-12-09 17:10:36 +0100

    Raised the call stack limit on Eval

--- bin/which.ps1
+++ bin/which.ps1
@@ -1,4 +1,4 @@
 #!/usr/bin/env pwsh
 Set-StrictMode -Version Latest
 Set-Location (Split-Path $PSScriptRoot)
-haxe share/build.hxml --run Run @args
+haxe share/build.hxml --define eval-call-stack-depth=1500 --run Run @args
