7b55282991369c763fd1c44c886548a657f8f3af
Cédric Belin <cedric@belin.io>
2020-12-10 10:54:01 +0100

    Code formatting

--- example/main.hx
+++ example/main.hx
@@ -9,6 +9,6 @@ class Example {
 		"foobar".which().first().handle(outcome -> switch outcome {
 			case Success(path): Sys.println('The "foobar" command is located at: $path');
 			case Failure(error): Sys.println(error.message);
-	});
+		});
 	}
 }
