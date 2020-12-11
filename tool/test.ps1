95a656fd44061c72e30deed0519bf0f3dabb907f
Cédric Belin <cedric@belin.io>
2020-12-11 23:36:59 +0100

    Added support for JVM testing

--- tool/test.ps1
+++ tool/test.ps1
@@ -2,7 +2,7 @@
 Set-StrictMode -Version Latest
 Set-Location (Split-Path $PSScriptRoot)
 
-foreach ($item in "eval", "hl", "js", "php") {
+foreach ($item in "eval", "hl", "js", "jvm", "php") {
 	Write-Host "> Testing '$item' target..."
 	haxe "test_$item.hxml"
 }
