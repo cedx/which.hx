c8015b50e82f32a8b483c5f6b323d553b0bec1cc
Cédric Belin <cedric@belin.io>
2020-12-11 15:18:39 +0100

    Updated the documentation

--- lib/php/adapter.php
+++ lib/php/adapter.php
@@ -3,9 +3,9 @@ namespace which;
 
 /**
  * Finds the instances of the specified `command` in the system path.
- * @param string $command The command to be resolved.
- * @param array $options Options used to customize the function behavior.
- * @return _FinderStream An object providing the stream of search results.
+ * @param $command The command to be resolved.
+ * @param $options Options used to customize the function behavior.
+ * @return An object providing the stream of search results.
  * @throws FinderException The specified command was not found.
  */
 function which(string $command, array $options = []): _FinderStream {
@@ -17,13 +17,13 @@ class _FinderStream {
 
 	/**
 	 * Creates a new finder stream.
-	 * @param FinderStream $stream The object providing the stream of search results.
+	 * @param $stream The object providing the stream of search results.
 	 */
 	function __construct(private FinderStream $stream) {}
 
 	/**
 	 * Returns all instances of the searched command.
-	 * @return string[] All instances of the searched command.
+	 * @return All instances of the searched command.
 	 * @throws FinderException The command was not found.
 	 */
 	function all(): array {
@@ -40,7 +40,7 @@ class _FinderStream {
 
 	/**
 	 * Returns the first instance of the searched command.
-	 * @return string The first instance of the searched command.
+	 * @return The first instance of the searched command.
 	 * @throws FinderException The command was not found.
 	 */
 	function first(): string {
