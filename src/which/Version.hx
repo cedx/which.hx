9056dbf69aaa8e062c7c2da36f634825b139dfea
Cédric Belin <cedric@belin.io>
2021-10-09 18:52:57 +0200

    Improved the `Version` class

--- src/which/Version.hx
+++ src/which/Version.hx
@@ -1,39 +1,36 @@
+#if macro
 import haxe.Json;
 import haxe.macro.Context;
-
-#if macro
 import sys.io.File;
 import sys.io.Process;
 #end
 
 /** Provides information about the program version. **/
-class Version {
+@:noDoc abstract class Version {
 
 	/** Gets the hash of the current Git commit. **/
-	macro public static function getGitCommitHash(): ExprOf<String> {
+	macro public static function getGitCommitHash() {
 		#if display
 			return macro $v{""};
 		#else
 			final process = new Process("git", ["rev-parse", "HEAD"]);
-			final hash = process.exitCode() != 0 ? process.stdout.readLine() : "ERROR";
+			final hash = process.exitCode() == 0 ? process.stdout.readLine() : process.stderr.readLine();
 			process.close();
 			return macro $v{hash};
 		#end
 	}
 
 	/** Gets the name of the Haxe target. **/
-	macro public static function getHaxeTarget(): ExprOf<String>
-		return macro $v{Context.definedValue("target.name")};
+	macro public static function getHaxeTarget() return macro $v{Context.definedValue("target.name")};
 
 	/** Gets the version of the Haxe compiler. **/
-	macro public static function getHaxeVersion(): ExprOf<String>
-		return macro $v{Context.definedValue("haxe")};
+	macro public static function getHaxeVersion() return macro $v{Context.definedValue("haxe")};
 
 	/** Gets the package version of this program. **/
-	macro public static function getPackageVersion(): ExprOf<String>
+	macro public static function getPackageVersion()
 		#if display
 			return macro $v{"0.0.0"};
 		#else
-			return macro $v{try Json.parse(File.getContent("haxelib.json")).version catch (e) "0.0.0"};
+			return macro $v{Json.parse(File.getContent("haxelib.json")).version};
 		#end
 }
