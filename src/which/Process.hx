847868e2e376add7408090d288b96898559c725e
Cédric Belin <cedric@belin.io>
2021-10-09 22:36:50 +0200

    Removed the `ProcessIdentity` enumeration

--- src/which/Process.hx
+++ src/which/Process.hx
@@ -5,16 +5,8 @@ import asys.io.Process as AsyncProcess;
 using StringTools;
 using tink.io.Source;
 
-#if nodejs
-import js.Lib.typeof;
-import js.Node;
-#elseif php
-import php.Global.function_exists;
-import php.Syntax;
-#end
-
 /** Provides information about the current process. **/
-class Process {
+abstract class Process {
 
 	/** The identifier of the current process's group. **/
 	public static var gid(get, never): Promise<Int>;
@@ -23,28 +15,20 @@ class Process {
 	public static var uid(get, never): Promise<Int>;
 
 	/** Gets the identifier of the current process's group. **/
-	static function get_gid(): Promise<Int> {
-		#if nodejs if (typeof(Node.process.getgid) == "function") return Node.process.getgid();
-		#elseif php if (function_exists("posix_getgid")) return Syntax.code("posix_getgid()"); #end
-		return getProcessId(Group);
-	}
+	static inline function get_gid() return getProcessId("g");
 
 	/** Gets the identifier of the current process's user. **/
-	static function get_uid(): Promise<Int> {
-		#if nodejs if (typeof(Node.process.getuid) == "function") return Node.process.getuid();
-		#elseif php if (function_exists("posix_getuid")) return Syntax.code("posix_getuid()"); #end
-		return getProcessId(User);
-	}
+	static inline function get_uid() return getProcessId("u");
 
-	/** Gets the numeric `identity` of the current process. **/
-	static function getProcessId(identity: ProcessIdentity): Promise<Int> {
-		if (Finder.isWindows) return Failure(new Error(MethodNotAllowed, "Not supported on Windows platform."));
+	/** Gets the numeric identity of the current process. **/
+	static function getProcessId(identity: String) {
+		if (Finder.isWindows) return Promise.reject(new Error(MethodNotAllowed, "Not supported on Windows platform."));
 
 		final process = new AsyncProcess("id", ['-$identity']);
 		return process.exitCode()
-			.next(exitCode -> exitCode != 0 ? Failure(new Error("Process exited with a non-zero code.")) : process.stdout.all().next(chunk -> {
-				final id = Std.parseInt(chunk.toString().rtrim());
-				id != null ? Success(id) : Failure(new Error("Unable to parse the process output."));
+			.next(exitCode -> exitCode != 0 ? new Error('Process exited with a $exitCode code.') : process.stdout.all().next(chunk -> {
+				final processId = Std.parseInt(chunk.toString().trim());
+				processId != null ? processId : new Error("Unable to parse the process output.");
 			}))
 			.next(processId -> {
 				process.close();
