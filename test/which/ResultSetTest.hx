fd89499612399f773294d98e3ba7dd437fd49758
Cédric Belin <cedric@belin.io>
2021-10-10 13:49:16 +0200

    Added new unit tests

--- test/which/ResultSetTest.hx
+++ test/which/ResultSetTest.hx
@@ -10,12 +10,11 @@ using StringTools;
 	public function new() {}
 
 	/** Tests the `all()` method. **/
-	/*
 	@:variant("executable", which.Finder.isWindows ? "\\test\\fixtures\\executable.cmd" : null)
 	@:variant("executable.sh", which.Finder.isWindows ? null : "/test/fixtures/executable.sh")
 	@:variant("foo", null)
-	public function testAll(command: String, output: Null<String>) {
-		which(command, {path: ["test/fixtures"]}).all().handle(outcome -> switch outcome {
+	public function testAll(input: String, output: Null<String>) {
+		which(input, {path: ["test/fixtures"]}).all().handle(outcome -> switch outcome {
 			case Success(paths): output != null
 				? { asserts.assert(paths.length == 1); asserts.assert(paths[0].endsWith(output)); asserts.done(); }
 				: asserts.fail("Promise not rejected.");
@@ -25,25 +24,22 @@ using StringTools;
 		});
 
 		return asserts;
-	}*/
+	}
 
 	/** Tests the `first()` method. **/
-	/*
 	@:variant("executable", which.Finder.isWindows ? "\\test\\fixtures\\executable.cmd" : null)
 	@:variant("executable.sh", which.Finder.isWindows ? null : "/test/fixtures/executable.sh")
 	@:variant("foo", null)
-	public function testFirst(command: String, output: Null<String>) {
-		which(command, {path: ["test/fixtures"]}).first().handle(outcome -> switch outcome {
-			case Success(path): output != null
-				? { asserts.assert(path.endsWith(output)); asserts.done(); }
-				: asserts.fail("Promise not rejected.");
-			case Failure(error): output != null
-				? asserts.fail(error.message)
-				: { asserts.assert(error.code == NotFound); asserts.done(); }
+	public function testFirst(input: String, output: Null<String>) {
+		which(input, {path: ["test/fixtures"]}).first().handle(outcome -> switch outcome {
+			case Success(path):
+				output != null ? { asserts.assert(path.endsWith(output)); asserts.done(); } : asserts.fail("Promise not rejected.");
+			case Failure(error):
+				output != null ? asserts.fail(error.message) : { asserts.assert(error.code == NotFound); asserts.done(); }
 		});
 
 		return asserts;
-	}*/
+	}
 
 	/** Tests the `stream()` method. **/
 	@:variant("executable", which.Finder.isWindows ? "\\test\\fixtures\\executable.cmd" : null)
