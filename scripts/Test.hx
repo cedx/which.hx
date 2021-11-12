a41af5f6c289fb2905eb3f2446ee4fa7499defb7
Cédric Belin <cedric@belin.io>
2021-11-12 12:25:57 +0100

    Replaced GitHub Actions by Bitbucket Pipelines for the CI

--- scripts/Test.hx
+++ scripts/Test.hx
@@ -1,7 +1,7 @@
 import Sys.*;
 
 /** Runs the script. **/
-function main() for (file in ["hl", "js", "jvm", "php"]) {
+function main() for (file in ["hl", "java", "js", "php"]) {
 	println('> Testing "$file" target...');
 	command('haxe test_$file.hxml');
 }
