dd9dcacf0de17b2988e65f2f3675848a9241c030
Cédric Belin <cedric@belin.io>
2022-01-15 01:11:58 +0100

    Fixed the default CLI command

--- src/which/Program.hx
+++ src/which/Program.hx
@@ -28,7 +28,7 @@ using Lambda;
 	@:defaultCommand
 	public function run(rest: Rest<String>): Promise<Noise> {
 		if (help || version) {
-			Sys.println(help ? Cli.getDoc(this) : Version.packageVersion);
+			Sys.println(version ? Version.packageVersion : Cli.getDoc(this));
 			return Noise;
 		}
 
