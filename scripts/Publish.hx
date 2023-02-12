0ee583be2ff8c20b1318b90c61d94911a6ec5cac
Cédric Belin <cedric@belin.io>
2023-02-12 01:29:41 +0100

    Fixed the "Publish" task [skip ci]

--- scripts/Publish.hx
+++ scripts/Publish.hx
@@ -6,5 +6,5 @@ function main() {
 	Sys.command("haxe", ["run.hxml"]);
 	Tools.compress(["CHANGELOG.md", "LICENSE.md", "README.md", "haxelib.json", "run.n", "src"], "var/haxelib.zip");
 	Sys.command("haxelib", ["submit", "var/haxelib.zip"]);
-	for (action in ["tag", "push origin"]) Sys.command("git", [action, 'v${Version.packageVersion}'])
+	for (action in [["tag"], ["push", "origin"]]) Sys.command("git", action.concat(['v${Version.packageVersion}']);
 }
