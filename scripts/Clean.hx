85185375f1cd48415f99af2e89410eba907f94e7
Cédric Belin <cedric@belin.io>
2023-04-10 19:25:28 +0200

    Update the PHP tests

--- scripts/Clean.hx
+++ scripts/Clean.hx
@@ -3,7 +3,6 @@ using Lambda;
 
 /** Deletes all generated files. **/
 function main() {
-	if (FileSystem.exists("test/index.php")) FileSystem.deleteFile("test/index.php");
 	["lib", "res"].filter(FileSystem.exists).iter(Tools.removeDirectory);
 	Tools.cleanDirectory("var");
 }
