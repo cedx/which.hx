64a55b6c024920c83f6ef02e641cfa2eb872885f
Cédric Belin <cedric@belin.io>
2023-04-17 11:07:08 +0200

    Update the "Doc" task [skip ci]

--- scripts/Doc.hx
+++ scripts/Doc.hx
@@ -11,7 +11,7 @@ function main() {
 	Sys.command("lix", ["run", "dox",
 		"--define", "description", "Find the instances of an executable in the system path, in Haxe.",
 		"--define", "source-path", "https://github.com/cedx/which.hx/blob/main/src",
-		"--define", "themeColor", "0xffc105",
+		"--define", "themeColor", "0xea8220",
 		"--define", "version", Version.packageVersion,
 		"--define", "website", "https://github.com/cedx/which.hx",
 		"--input-path", "var",
