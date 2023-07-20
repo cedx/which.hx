4920d5355ddb6d7fd5ead926c0fa8665c1f705ef
Cédric Belin <cedric@belin.io>
2023-07-20 21:14:07 +0200

    Update the examples

--- example/Stream.hx
+++ example/Stream.hx
@@ -3,7 +3,10 @@ import tink.streams.Stream.Handled;
 using tink.CoreApi;
 
 /** Finds all instances of an executable and returns them one at a time. */
-function main() which("foobar").stream().forEach(path -> {
-	Sys.println('The "foobar" command is located at: $path');
-	Handled.Resume;
-});
+function main() {
+	Sys.println('The "foobar" command is available at these locations:');
+	which("foobar").stream().forEach(path -> {
+		Sys.println('- $path');
+		Handled.Resume;
+	});
+}
