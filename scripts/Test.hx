7adf3479f740f032471c17066c78125f159bab86
Cédric Belin <cedric@belin.io>
2023-07-23 18:38:54 +0200

    Code optimization

--- scripts/Test.hx
+++ scripts/Test.hx
@@ -1,9 +1,6 @@
 /** Runs the test suite. **/
-function main() {
-	var exitCode = 0;
-	for (file in ["hl", "java", "js", "php"]) {
-		Sys.println('> Testing the "$file" target...');
-		exitCode = Sys.command('haxe test_$file.hxml');
-		if (exitCode != 0) Sys.exit(exitCode);
-	}
+function main() for (file in ["hl", "java", "js", "php"]) {
+	Sys.println('> Testing the "$file" target...');
+	var exitCode = Sys.command('haxe test_$file.hxml');
+	if (exitCode != 0) Sys.exit(exitCode);
 }
