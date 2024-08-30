fdd59b79dfdb063d03aa8c4b88e28b9281455e64
Cédric Belin <cedric@belin.io>
2024-08-30 23:42:48 +0200

    Code optimization

--- src/which/Process.hx
+++ src/which/Process.hx
@@ -4,10 +4,9 @@ package which;
 import js.Node;
 #elseif php
 import php.Syntax;
-#else
-import asys.io.Process as AsysProcess;
 #end
 
+import asys.io.Process as AsysProcess;
 using StringTools;
 using tink.io.Source;
 
@@ -16,29 +15,30 @@ abstract class Process {
 
 	/** The identifier of the current process's group. **/
 	public static var gid(get, never): Promise<Int>;
-		static inline function get_gid() return getProcessId("g");
+		static inline function get_gid(): Promise<Int>
+			return Finder.isWindows ? new Error(MethodNotAllowed, "Not supported on Windows platform.") :
+				#if js Node.process.getgid()
+				#elseif php Syntax.code("posix_getgid()")
+				#else getProcessId("g") #end;
 
 	/** The identifier of the current process's user. **/
 	public static var uid(get, never): Promise<Int>;
-		static inline function get_uid() return getProcessId("u");
+		static inline function get_uid(): Promise<Int>
+			return Finder.isWindows ? new Error(MethodNotAllowed, "Not supported on Windows platform.") :
+				#if js Node.process.getuid()
+				#elseif php Syntax.code("posix_getuid()")
+				#else getProcessId("u") #end;
 
-	/** Gets the numeric identity of the current process. **/
+	/** Gets the numeric identity of the current process by using the "id" command. **/
 	static function getProcessId(identity: String): Promise<Int> {
-		if (Finder.isWindows) return Promise.reject(new Error(MethodNotAllowed, "Not supported on Windows platform."));
-
-		#if nodejs
-			return Promise.resolve(identity == "g" ? Node.process.getgid() : Node.process.getuid());
-		#elseif php
-			return Promise.resolve(identity == "g" ? Syntax.code("posix_getgid()") : Syntax.code("posix_getuid()"));
-		#else
-			final process = new AsysProcess("id", ['-$identity']);
-			return process.exitCode()
-				.next(exitCode -> exitCode == 0 ? process.stdout.all() : Error.withData('The "id" process exited with a $exitCode code.', exitCode))
-				.next(stdout -> {
-					process.close();
-					final processId = Std.parseInt(stdout.toString().trim());
-					processId != null ? processId : new Error("Unable to parse the process output.");
-				});
-		#end
+		final process = new AsysProcess("id", ['-$identity']);
+		return process.exitCode()
+			.next(exitCode -> exitCode == 0 ? process.stdout.all() : Error.withData('The "id" command failed.', exitCode))
+			.next(stdout -> {
+				process.close();
+				final output = stdout.toString();
+				final processId = Std.parseInt(output.trim());
+				processId != null ? processId : Error.withData('Unable to parse the output of the "id" command.', output);
+			});
 	}
 }
