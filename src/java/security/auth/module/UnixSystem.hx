af5c195b1988f794e900d946ddce37c8d4d9337f
Cédric Belin <cedric@belin.io>
2024-09-11 15:07:54 +0200

    Fix the Java externs

--- src/java/security/auth/module/UnixSystem.hx
+++ src/java/security/auth/module/UnixSystem.hx
@@ -1,5 +1,7 @@
 package java.security.auth.module;
 
+import java.lang.Long;
+
 /** Retrieves and makes available Unix UID/GID/groups information for the current user. **/
 @:native("com.sun.security.auth.module.UnixSystem")
 extern class UnixSystem {
@@ -8,8 +10,8 @@ extern class UnixSystem {
 	function new();
 
 	/** Gets the GID for the current Unix user. **/
-	function getGid(): Int;
+	function getGid(): Long;
 
 	/** Gets the UID for the current Unix user. **/
-	function getUid(): Int;
+	function getUid(): Long;
 }
