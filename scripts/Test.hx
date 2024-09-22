2c6c734d3b2f42eb706f90b61567b527a532d453
Cédric Belin <cedric@belin.io>
2024-09-22 15:46:32 +0200

    Update the CI

--- scripts/Test.hx
+++ scripts/Test.hx
@@ -1,7 +1,7 @@
 /** Runs the test suite. **/
 function main() {
 	var exitCode = 0;
-	for (file in ["hl", "java", "js", "php"]) {
+	for (file in ["hl", "java", "nodejs", "php"]) {
 		Sys.println('> Testing the "$file" target...');
 		if (Sys.command('haxe test_$file.hxml') != 0) exitCode++;
 	}
