de1a8086739b24908a03a9afe11567ae7bfce4a1
Cédric Belin <cedric@belin.io>
2024-09-22 18:25:41 +0200

    Enable the "eval" compilation target

--- scripts/Test.hx
+++ scripts/Test.hx
@@ -1,7 +1,7 @@
 /** Runs the test suite. **/
 function main() {
 	var exitCode = 0;
-	for (file in ["hl", "java", "nodejs", "php"]) {
+	for (file in ["eval", "hl", "java", "nodejs", "php"]) {
 		Sys.println('> Testing the "$file" target...');
 		if (Sys.command('haxe test_$file.hxml') != 0) exitCode++;
 	}
